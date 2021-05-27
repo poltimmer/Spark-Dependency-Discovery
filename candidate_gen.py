f1c98f749d9850629dfc709164b2a4a5561979e0
Jacco <jaccokiezebrink@gamil.com>
2021-05-27 21:50:32 +0200

    timings enzo

--- candidate_gen.py
+++ candidate_gen.py
@@ -2,15 +2,17 @@ import random
 from DependencyFinder import setup_header, soft_fd
 import itertools
 import pickle
+import time
 
 TAU = 0.98
-FILE_PATH = "/home/tommie/Downloads/VR_20051125_Post"
-DELTA = 2 #can be done
+FILE_PATH = "/home/mcs001/shared_data/mcs-2amd15/mcs-2amd15-15/VR_20051125_Post"
+DELTA = 2  # can be done
+
 
 def main():
     header, input_rdd = setup_header(FILE_PATH)
-    columns = list(header.split('\t'))#[:2] #TODO remove [:5]
- 
+    columns = list(header.split('\t'))[:2]  # TODO remove [:5]
+
     print(columns)
     min_fd = {}
     for col in columns:
@@ -20,8 +22,9 @@ def main():
     for col in columns:
         min_soft_fd[col] = []
 
+    initial_time = time.time()
     for depth in range(1, 4):
-        # TODO: add a check and to break if there are no candidates 
+        # TODO: add a check and to break if there are no candidates
         perms = {}
         # Create permutations for current layer
         for col in columns:
@@ -32,20 +35,25 @@ def main():
                 if all(map(lambda x: not set(x).issubset(set(candidate[0])), map(lambda fd: fd[0], min_fd[key]))):
                     candidates.append(candidate)
 
-        print('checking p values for {} candidates'.format(len(candidates)))
-        p_values_map = soft_fd(candidates, columns, input_rdd) # [((('A'), 'B'), 0.97)]
-        print('p values map for iter {}: {}'.format(depth, p_values_map))
+        print('>> checking p values for {} candidates ||'.format(len(candidates)))
+        start_time = time.time()
+        p_values_map = soft_fd(candidates, columns, input_rdd)  # [((('A'), 'B'), 0.97)]
+        print(">> Runtime: {}".format(time.time() - start_time))
+        print('>> p values map for iter {}: {} ||'.format(depth, p_values_map))
 
         # Build min_fd and min_soft_fd lists
         for (lhs, rhs), p_val in p_values_map:
-            if p_val == 1: # case 1: hard functional dependency
+            if p_val == 1:  # case 1: hard functional dependency
                 min_fd[rhs].append((lhs, rhs))
             elif p_val >= TAU:  # case 2: soft functional dependency
                 if all(map(lambda x: not set(x).issubset(set(lhs)), map(lambda fd: fd[0], min_soft_fd[key]))):
                     min_soft_fd[rhs].append((lhs, rhs))
 
-        # print(min_fd)
-        # print(min_soft_fd)
+    print(">> Total runtime: {} ||".format(time.time() - initial_time))
+
+    # print(min_fd)
+    # print(min_soft_fd)
+
 
 if __name__ == '__main__':
     main()
