2105ecdc8c885ba8a0a6635f3d598c38d62c00dc
Tommie Kerssies <tommie.kerssies@marketredesign.com>
2021-06-03 09:58:39 +0200

    wip

--- candidate_gen.py
+++ candidate_gen.py
@@ -1,7 +1,7 @@
 from DependencyFinder import setup_header, soft_fd, delta_fd
 import itertools
 import time
-
+from warnings import warn
 FILE_PATH = "/group/mcs-2amd15-15/VR_20051125_Post"  # <-- file path when using hpc
 
 
@@ -34,8 +34,35 @@ def main(nr_of_columns, nr_of_rows=None, tau=None, delta=None):
             # print('>> checking p values for {} candidates ||'.format(len(candidates)))
             # start_time = time.time()
 
-            sample_fraction = 0.01  # TODO: 1% example fraction, replace by your own
-            p_values_map = soft_fd(candidates, columns, input_rdd, sample_fraction)  # [((('A'), 'B'), 0.97)]
+            next_gen_candidates = list(candidates)
+            
+            # iterative sampling
+            for factor in [2, 5, 10, 25]:
+                frac = factor*2*(1-tau)
+                if frac >= 1:
+                    warn(f"[warning] - Sampling fraction {frac} below minimum threshold given tau ({tau}), skipping fraction")
+                    continue
+                distance_values_map = soft_fd(next_gen_candidates, delta, columns, input_rdd, frac)  # [((('A'), 'B'), 2)]
+                next_gen_candidates = []
+                for (lhs, rhs), p_val in p_values_map:
+                    # p-val is 0.9
+                    # tau is 0.95
+                    # frac is 1*2*(1-0.95)=0.1
+                    # tau / frac = 9.5
+                    # 0.9 > 9.5 = false
+
+                    # 1
+
+
+                    # tau = 0.99 (klopt de dependency in 99% van de data?)
+                    # 1-tau = 0.01 (in maximimaal 1% van de data mag het niet kloppen)
+                    # frac =  0.1 (we samplen 10% van de data)
+                    # p_val >= 1 - ((1-tau) * (fraction)) (als dit waar is, dan kunnen we hem niet uitsluiten)
+                    # 10% 1% -> 
+                    if p_val >= 1 - ((1-tau) * (frac)):
+                        next_gen_candidates.append((lhs, rhs))
+
+            p_values_map = soft_fd(next_gen_candidates, input_rdd)  # [((('A'), 'B'), 0.97)]
             # print(">> Runtime: {}".format(time.time() - start_time))
             # print('>> p values map for iter {}: {} ||'.format(depth, p_values_map))
 
@@ -66,9 +93,15 @@ def main(nr_of_columns, nr_of_rows=None, tau=None, delta=None):
                            and all(map(lambda x: not set(x).issubset(set(candidate[0])), map(lambda d_fd: d_fd[0], min_delta_fd[key]))):
                         candidates.append(candidate)
 
-            sample_fraction = 0.01  # TODO: 1% example fraction, replace by your own
-            distance_values_map = delta_fd(candidates, delta, columns, input_rdd, sample_fraction)  # [((('A'), 'B'), 2)]
+            next_gen_candidates = list(candidates)
+            for frac in [0.001, 0.01, 0.05, 0.10]:
+                distance_values_map = delta_fd(next_gen_candidates, delta, columns, input_rdd, frac)  # [((('A'), 'B'), 2)]
+                next_gen_candidates = []
+                for (lhs, rhs), dist in distance_values_map:
+                    if dist <= delta:
+                        next_gen_candidates.append((lhs, rhs))
 
+            delta_fd(next_gen_candidates, delta, columns, input_rdd)
             # Build min_fd and min_soft_fd lists
             for (lhs, rhs), dist in distance_values_map:
                 if dist <= delta:
@@ -79,7 +112,7 @@ def main(nr_of_columns, nr_of_rows=None, tau=None, delta=None):
 
 
 if __name__ == '__main__':
-    default_tau = 0.98
+    default_tau = 0.995
     default_delta = 1
     main(nr_of_columns=5, tau=default_tau, delta=default_delta)
 
